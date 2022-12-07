f867afd789ece8b22c29fb8a3861c17afb51e5cd
Shem Baijoo <shem.baijoo@gmail.com>
2022-12-07 15:30:13 -0500

    API updates

--- StuMSAPI/StuMSAPI/Data/ApplicationDbContext.cs
+++ StuMSAPI/StuMSAPI/Data/ApplicationDbContext.cs
@@ -12,5 +12,32 @@ namespace StuMSAPI.Data
 
         public DbSet<Student> Students { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    Id = 1,
+                    Name = "Nick",
+                    Dob = new DateOnly(1999, 12, 9),
+                    Email = "nick@junkie.com"
+                },
+              new Student
+              {
+                  Id = 2,
+                  Name = "Momo",
+                  Dob = new DateOnly(2000, 12, 10),
+                  Email = "momo@junkie.com"
+              },
+              new Student
+              {
+                  Id = 3,
+                  Name = "Shem",
+                  Dob = new DateOnly(2001, 12, 11),
+                  Email = "shem@junkie.com"
+              });
+        }
+
     }
 }
